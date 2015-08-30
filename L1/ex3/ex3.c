9b3caddfed2230394dde92266b5cf3b0f83bb491
Eugene <yeo.eugene.oey@gmail.com>
2015-08-30 23:35:11 +0800

    Exercise 3 Lab1

--- L1/ex3/ex3.c
+++ L1/ex3/ex3.c
@@ -1,53 +1,56 @@
 /*************************************
 * Lab 1 Exercise 3
-* Name     :
-* Matric No:
-* Lab Group:
+* Name     : Eugene
+* Matric No: A0116631N
+* Lab Group: 8
 *************************************/
 
 #include <stdio.h>
 
-//Datatype Declarations
+// Aliases
 typedef int (*arithFuncPtr)(int, int);
 
+// Function Prototype 
+// (Use leading underscore to avoid conflict with the built in keyword)
+int _add(int x, int y);
+int _sub(int x, int y);
+int _mul(int x, int y);
+int _div(int x, int y);
+int _zero(int x, int y);
 
-//Function Prototypes
-int add(int x, int y);
+int main(){
+	int x, y, op, result;
+	arithFuncPtr opPtr;
+	while (scanf("%i %i %i", &x, &y, &op) == 3){
+		switch (op){
+			case 1: opPtr = _add; break;
+			case 2: opPtr = _sub; break;
+			case 3: opPtr = _mul; break;
+			case 4: opPtr = _div; break;
+            default: opPtr = _zero; break;
+		}
+		result = (*opPtr)(x, y);
+		printf("%i\n", result);
+	}
+	return 0;
+}
 
+int _add(int x, int y){
+	return x + y;
+}
 
-int main()
-{
-    int a, b, optype, res;
+int _sub(int x, int y){
+	return x - y;
+}
 
-    //The code below is to show the usage of a function pointer only
-    //You can remove/comment them off 
+int _mul(int x, int y){
+	return x * y;
+}
 
-    //arithFuncPtr is a function pointer datatype
-    // it points to afunction that takes (int, int) as parameter
-    //  and returns int as return result
-    arithFuncPtr ptr;
+int _div(int x, int y){
+	return x / y;
+}
 
-    //ptr points to the function add
-    ptr = add;
-
-    scanf("%i %i", &a, &b);
-
-    //Defereference a function pointer === call the function
-    //if you take (*ptr) as some function f
-    // then below is the same as
-    //   res = f(a, b);
-
-    res = (*ptr)(a, b);
-
-    printf("%i\n", res);
-   
+int _zero(int x, int y){
     return 0;
 }
-
-int add(int x, int y)
-{
-    return x+y;
-}
-
-
-
